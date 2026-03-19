c38af8d5028b9411b0b5df66a331c5fe9e993861
izauemir-create <iza.u.emir@gmail.com>
2026-03-19 16:46:01 +0600

    6 Home Work

--- project/project/js/lesson.js
+++ project/project/js/lesson.js
@@ -39,3 +39,39 @@ eurInput.addEventListener('input', () => {
   somInput.value = som.toFixed(2)
   usdInput.value = (som / SOM_PER_USD).toFixed(4)
 });
+
+// CARD SWITCHER
+
+const cardBlock = document.querySelector('.card');
+const btnNext = document.querySelector('#btn-next');
+const btnPrev = document.querySelector('#btn-prev');
+
+const TOTAL = 200;
+let cardId = 1;
+
+function fetchCard(id) {
+  return fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
+    .then(r => r.json());
+}
+
+function renderCard(data) {
+  const { title, id, completed } = data;
+  cardBlock.innerHTML = `
+    <p>${id} / ${TOTAL}</p>
+    <p>${title}</p>
+    <p>${completed}</p>
+  `;
+}
+
+function navigate(dir) {
+  cardId += dir;
+  if (cardId < 1) cardId = TOTAL;
+  if (cardId > TOTAL) cardId = 1;
+  fetchCard(cardId).then(renderCard);
+}
+
+btnNext.onclick = () => navigate(1);
+btnPrev.onclick = () => navigate(-1);
+
+// Карточка не пустая при загрузке
+fetchCard(cardId).then(renderCard);
